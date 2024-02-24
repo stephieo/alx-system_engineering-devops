610e1b733d2abc33b2b9d9c5ed004cd075db912f
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-02-24 01:39:27 +0100

    fix: syntax and style error

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # This is a manifest specifying the creation of a file
 
-file { 'school'
-    path => '/tmp/school',
-    mode => '0744',
-    owner => 'www-data',
-    group => 'www-data',
+file { 'school':
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
     content => 'I love Puppet'
 }
