ed186b9d44c930fcecce1c1f7e6eb41551f281ca
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-03-06 05:24:20 +0100

    feat: puppet configuration of nginx server

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,28 @@
+exec { 'apt-update':
+  command => '/usr/bin/apt-get -y update',
+  path    => ['/usr/bin', '/bin'],
+}
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+file_line { 'add custom header':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => "\tadd_header X-Served-By ${hostname};",
+  after  => 'server_name _;',
+}
+
+exec { 'restart_server':
+  provider => shell,
+  command  => '/usr/sbin/service nginx restart',
+}
+
+service { 'nginx':
+  ensure => running,
+}
