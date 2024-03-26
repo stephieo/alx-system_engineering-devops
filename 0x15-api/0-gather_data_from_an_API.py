a63d6d6533dcd4555b62060223a7b4d7f898f7fc
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-03-26 21:37:35 +0100

    feat: python script using test API (incomplete)

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" returns information about employee TODO list progress"""
+from sys import argv
+import requests
+
+#api_url = "https://jsonplaceholder.typicode.com/todos/"
+
+#full_url = api_url + argv[1]
+
+
+#response = requests.get(full_url)
+#print(response)
+#response2 = requests.get("https://jsonplaceholder.typicode.com/users/3")
+#print(response2.json())
+#response.dict = response.json()
+#print(response.dict)
+user_collection = "https://jsonplaceholder.typicode.com/users/"
+user_data = user_collection + argv[1]
+
+response = (requests.get(user_data)).json()
+EMPLOYEE_NAME = response['name']
+
+user_todos = f"https://jsonplaceholder.typicode.com/users/{argv[1]}/todos"
+todo_response = (requests.get(user_todos)).json()
+
+NUMBER_OF_DONE_TASKS = 0
+TOTAL_NUMBER_OF_TASKS = 0
+for item in todo_response:
+    if item['completed'] == True:
+        NUMBER_OF_DONE_TASKS += 1
+    TOTAL_NUMBER_OF_TASKS += 1
+
+print(f"Employee {EMPLOYEE_NAME} is done with tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+for item in todo_response:
+    if item['completed'] == True:
+        print(f"\t{item['title']}")
+#NUMBER_OF_DONE_TASKS = todo_response[]
+#fetch(full_url)
+#      .then(response => response.json())
+#      .then(json => console.log(json))
