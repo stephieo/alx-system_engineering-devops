8b8afdf33942232f3237b4db8fb67bfd83592ce5
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-03-27 11:42:20 +0100

    feat: export API data to csv file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+""" returns information about employee TODO list progress """
+import csv
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    # accessing api with user task data
+    user_todos = f"https://jsonplaceholder.typicode.com/users/{argv[1]}/todos"
+    todo_response = (requests.get(user_todos)).json()
+
+    # writing to csv file
+    with open(f"{argv[1]}.csv", 'w') as file:
+        cfile = csv.writer(file, delimiter=',', quotechar='"',
+                           quoting=csv.QUOTE_ALL)
+        for item in todo_response:
+            cfile.writerow([item['userId'], item['completed'], item['title']])
