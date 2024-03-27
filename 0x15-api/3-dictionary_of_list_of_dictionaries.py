77839a43cabc6ddd20a5b93ab054d8f5ae47889c
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-03-27 21:32:30 +0100

    fix: corected output layout

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -5,16 +5,16 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    task_list = []
     task_dict = {}
     user_dict = {}
-    employee_dict = {}
+    all_employees = {}
 
     user_data = "https://jsonplaceholder.typicode.com/users"
     # accessing the data of all users
     users = (requests.get(user_data)).json()
 
     for user in users:
+        task_list = []
         # access each users task data
         user_todos = user_data + f"/{user['id']}/todos"
 
@@ -25,7 +25,7 @@ if __name__ == "__main__":
                          "completed": item['completed']}
             task_list.append(task_dict)
         # update master dictionary
-        employee_dict[user['id']] = task_list
+        all_employees[user['id']] = task_list
 
     with open(f"todo_all_employees.json", 'w') as file:
-        json.dump(employee_dict, file)
+        json.dump(all_employees, file)
