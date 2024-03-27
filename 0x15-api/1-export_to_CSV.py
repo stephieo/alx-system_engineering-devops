016ffab353f50b412bd46504c23b5185d186a917
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-03-27 11:51:17 +0100

    fix: username formatting

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,10 +8,16 @@ if __name__ == "__main__":
     # accessing api with user task data
     user_todos = f"https://jsonplaceholder.typicode.com/users/{argv[1]}/todos"
     todo_response = (requests.get(user_todos)).json()
-
+   
+    # accessing username
+    user_collection = "https://jsonplaceholder.typicode.com/users/"
+    user_data = user_collection + argv[1]
+    response = (requests.get(user_data)).json()
+    username = response['name']
+    
     # writing to csv file
     with open(f"{argv[1]}.csv", 'w') as file:
         cfile = csv.writer(file, delimiter=',', quotechar='"',
                            quoting=csv.QUOTE_ALL)
         for item in todo_response:
-            cfile.writerow([item['userId'], item['completed'], item['title']])
+            cfile.writerow([item['userId'], username, item['completed'], item['title']])
