423b1580c4000a39e6651f1836f5ff528fcd6a6c
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-03-27 09:31:25 +0100

    style: pycodestyle fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,17 +4,6 @@ from sys import argv
 import requests
 
 if __name__ == "__main__":
-    #api_url = "https://jsonplaceholder.typicode.com/todos/"
-
-    #full_url = api_url + argv[1]
-
-
-    #response = requests.get(full_url)
-    #print(response)
-    #response2 = requests.get("https://jsonplaceholder.typicode.com/users/3")
-    #print(response2.json())
-    #response.dict = response.json()
-    #print(response.dict)
     user_collection = "https://jsonplaceholder.typicode.com/users/"
     if argv[1]:
         user_data = user_collection + argv[1]
@@ -28,15 +17,12 @@ if __name__ == "__main__":
     NUMBER_OF_DONE_TASKS = 0
     TOTAL_NUMBER_OF_TASKS = 0
     for item in todo_response:
-        if item['completed'] == True:
+        if item['completed'] is True:
             NUMBER_OF_DONE_TASKS += 1
         TOTAL_NUMBER_OF_TASKS += 1
 
-    print(f"Employee {EMPLOYEE_NAME} is done with tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+    print(f"Employee {EMPLOYEE_NAME} is done with tasks
+          ({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}): ")
     for item in todo_response:
-        if item['completed'] == True:
+        if item['completed'] is True:
             print(f"\t{item['title']}")
-    #NUMBER_OF_DONE_TASKS = todo_response[]
-    #fetch(full_url)
-    #      .then(response => response.json())
-    #      .then(json => console.log(json))
