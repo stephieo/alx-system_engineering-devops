3b570efb61f7b9f911d404b285bc2c8db367544e
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-04-11 14:26:41 +0100

    feat: create function for reddit API function to return the number of subcribers in a given subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+""" script to query Reddit API and
+return the number of users in given subreddit
+"""
+import requests
+import requests.auth
+from client_secrets import USERNAME, PASSWORD, CLIENT_ID, CLIENT_SECRET
+import json
+
+
+def number_of_subscribers(subreddit):
+    """ return the  number of total subscribers
+        in a given subreddit
+    """
+    # Authenticating Reddit app
+    client_auth = requests.auth.HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
+    post_data = {
+        'grant_type': 'password',
+        'username': USERNAME,
+        'password': PASSWORD
+    }
+    headers = {
+        'User-Agent': 'Stephanie'
+    }
+
+    # Getting token Access Id
+    TOKEN_ACCESS_ENDPOINT = 'https://www.reddit.com/api/v1/access_token'
+    response = requests.post(TOKEN_ACCESS_ENDPOINT, data=post_data,
+                             headers=headers, auth=client_auth)
+    if response.status_code == 200:
+        token_id = response.json()['access_token']
+        headers['Authorization'] = f"bearer {token_id}"
+
+    # Query
+    OAUTH_ENDPOINT = "https://oauth.reddit.com"
+    response = requests.get(OAUTH_ENDPOINT + f"/r/{subreddit}/about",
+                            headers=headers)
+
+    if response.status_code != 200:
+        print("ERRORR!", response.status_code)
+        return 0
+
+    subscriber_count = response.json().get('data').get('subscribers')
+    return subscriber_count
---
+++ 0x16-api_advanced/client_secrets.py
@@ -0,0 +1,9 @@
+CLIENT_ID = 'RzlTJd_ntMrTA1n0mu0Xag'
+CLIENT_SECRET = 'DGZHkE558vMpHpscEqbZmDz6LSFb4g'
+
+
+redirect_uri  =  'http://localhost:8080'
+# refresh_token = 
+# user_agent = 
+USERNAME = 'ChocolateyCoolcat'
+PASSWORD = 'redd714552o'
