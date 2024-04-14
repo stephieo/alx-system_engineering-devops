ddc585a29181b813fb2596aa3bc667e23f3d3eda
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-04-14 20:04:00 +0100

    fixed quotation marks error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,6 @@
 # fix Apache error
 
 exec { 'Worpress fix':
-    command => 'sed -i s/phpp/php/g var/www/html/wp-settings.php',
+    command => 'sed -i "s/phpp/php/g" var/www/html/wp-settings.php',
     path    => '/bin'
-
 }
