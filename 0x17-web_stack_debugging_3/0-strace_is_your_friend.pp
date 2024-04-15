34fb83b07a3669022e61fa576a38cbe5208d14ca
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-04-15 16:06:37 +0100

    fix: typo in wordpress file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# fix Apache error
+# fix bad extension causing Apache error
 
 exec { 'Worpress fix':
-    provider => shell,
     command  => 'sed -i "s/phpp/php/g" var/www/html/wp-settings.php',
+    path     => 'usr/local/bin/:/bin/'
 }
