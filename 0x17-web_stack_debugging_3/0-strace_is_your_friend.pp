426124378d6752ff987d4587a12867fb3c9d109d
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-04-17 12:10:12 +0100

    fix: change exec name

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # fix bad extension causing Apache error
 
-exec { 'Worpress fix':
+exec { 'replace':
     provider => shell,
     command  => 'sed -i "s/phpp/php/g" var/www/html/wp-settings.php',
 }
