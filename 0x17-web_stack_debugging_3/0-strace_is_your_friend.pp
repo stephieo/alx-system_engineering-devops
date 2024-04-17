84174b9dc8bcbd818553358016cf04d123ff40bc
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-04-17 12:08:15 +0100

    fix: change puppet command

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # fix bad extension causing Apache error
 
 exec { 'Worpress fix':
-    command => 'sed -i "s/phpp/php/g" var/www/html/wp-settings.php',
-    path    => 'usr/local/bin/:/bin/'
+    provider => shell,
+    command  => 'sed -i "s/phpp/php/g" var/www/html/wp-settings.php',
 }
